0f0dbe39d228b4149badae481332c8eaebed703b
gugabatistella <gustavo_batistella@estudante.sesisenai.org.br>
2026-09-18 08:52:27 -0300

    Exercicio 13

---
+++ parte3-while/ex13.py
@@ -0,0 +1,5 @@
+senha = str(input("Digite a senha: "))
+while senha != "senai123":
+    print("Senha incorreta. Tente novamente.")
+    senha = str(input("Digite a senha: "))
+print("Acesso liberado.")
