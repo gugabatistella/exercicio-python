018c4ff0adf043ab71112e0058d8147569c54b81
gugabatistella <gustavo_batistella@estudante.sesisenai.org.br>
2026-09-18 08:31:49 -0300

    Exercicio 04

--- parte1-variaveis/ex04.py
+++ parte1-variaveis/ex04.py
@@ -1,4 +1,4 @@
 celsius = int(input("Digite a temperatura em Celsius: "))
 fahrenheit = (celsius * 9/5) + 32.5
 
-print("A temperatura em Fahrenheit é: ", fahrenheit)
+print("A temperatura em Fahrenheit é: ", f"{fahrenheit:.1f}")
