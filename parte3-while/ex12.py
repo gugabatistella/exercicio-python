6ed67f7d351929bf378e3d324312ed7566f87daf
gugabatistella <gustavo_batistella@estudante.sesisenai.org.br>
2026-09-18 08:48:03 -0300

    Exercicio 12

---
+++ parte3-while/ex12.py
@@ -0,0 +1,7 @@
+contador = int(input("Digite um número: "))
+opcao = contador
+while opcao != 0:
+    print(contador)
+    opcao = int(input("Digite outro número (ou 0 para sair): "))
+    contador = contador + opcao
+    print("A soma dos números digitados é: ", contador)
